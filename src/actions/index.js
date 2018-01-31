d2275a241c351f9d3ef87e312dfee92c61e39ed4
Koen Kivits <koen@kivits.com>
2018-01-31 16:41:21 +0100

    Move actions to separate file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,31 @@
-const actions = store => ({
-  setAnomaly: (state, anomaly) => ({
-    anomaly,
-    ...state,
-  }),
+export const toggleDisabled = ({ daltonizer, ...other }) => ({
+  ...other,
+  daltonizer: {
+    ...daltonizer,
+    disabled: !daltonizer.disabled,
+  },
 });
 
-export default actions;
+// TODO anomalies should not be part of actions
+// (to guarantee purity and stuff)
+const anomalies = [
+  "protanomaly",
+  "protanopia",
+  "deuteranomaly",
+  "deuteranopia",
+  "tritanomaly",
+  "tritanopia",
+];
+
+export const nextAnomaly = ({ daltonizer, ...other }) => {
+  const currentIndex = anomalies.indexOf(daltonizer.anomaly);
+  const nextAnomaly = anomalies[(currentIndex + 1) % anomalies.length];
+
+  return {
+    ...other,
+    daltonizer: {
+      ...daltonizer,
+      anomaly: nextAnomaly,
+    },
+  };
+};
--- src/containers/Daltonize.js
+++ src/containers/Daltonize.js
@@ -2,42 +2,12 @@ import { h } from "preact";
 import { connect } from "unistore/preact";
 
 import Daltonize from "../components/Daltonize";
-
-const anomalies = [
-  "protanomaly",
-  "protanopia",
-  "deuteranomaly",
-  "deuteranopia",
-  "tritanomaly",
-  "tritanopia",
-];
+import { nextAnomaly, toggleDisabled } from "../actions";
 
 const mapStateToProps = ({ daltonizer }) => ({
   ...daltonizer,
 });
-
-const actions = {
-  toggleDisabled: ({ daltonizer, ...other }) => ({
-    ...other,
-    daltonizer: {
-      ...daltonizer,
-      disabled: !daltonizer.disabled,
-    },
-  }),
-
-  nextAnomaly: ({ daltonizer, ...other }) => {
-    const currentIndex = anomalies.indexOf(daltonizer.anomaly);
-    const nextAnomaly = anomalies[(currentIndex + 1) % anomalies.length];
-
-    return {
-      ...other,
-      daltonizer: {
-        ...daltonizer,
-        anomaly: nextAnomaly,
-      },
-    };
-  },
-};
+const actions = { nextAnomaly, toggleDisabled };
 
 export default connect(mapStateToProps, actions)(
   ({ anomaly, nextAnomaly, toggleDisabled, children, ...otherProps }) => (
