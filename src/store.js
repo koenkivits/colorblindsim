1bc7b3a0edfa3c5d6558bfd3adb956123c05e163
Koen Kivits <koen@kivits.com>
2018-02-01 20:42:47 +0100

    Add Redux devtools support

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import createStore from "unistore";
+import devtools from "unistore/devtools";
 
-const store = createStore({
+const initialState = {
   daltonizer: {
     anomaly: "tritanopia",
     disabled: false,
   },
-});
+};
+
+let store = createStore(initialState);
+if (module.hot) {
+  store = devtools(store);
+}
 
 export default store;
