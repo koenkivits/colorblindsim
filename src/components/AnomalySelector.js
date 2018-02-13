a533cb92dcddf01e040a56d2f0ef067a9af36221
Koen Kivits <koen@kivits.com>
2018-02-14 00:23:59 +0100

    Add stats to anomaly selector

--- src/components/AnomalySelector.js
+++ src/components/AnomalySelector.js
@@ -3,6 +3,36 @@ import { h, Component } from "preact";
 import colorVisionData from "../../lib/daltonize/anomalies";
 import style from "./AnomalySelector.css";
 
+const formatStats = stats => {
+  if (!stats) {
+    return "very rare";
+  }
+
+  // TODO also long descriptions (for title)
+  let result = `${stats[0]}% of males`;
+  if (!stats[1]) {
+    return result;
+  } else if (stats[1] === true) {
+    return `${result} and females`;
+  } else {
+    return `${result}, ${stats[1]}% of females`;
+  }
+};
+
+const AnomalyOption = ({ value, anomaly, selected, onChange }) => (
+  <label class={selected ? "selected" : ""}>
+    <input
+      type="radio"
+      name="anomaly"
+      checked={selected}
+      value={value}
+      onChange={onChange}
+    />
+    {anomaly.name}
+    <small>{formatStats(anomaly.stats)}</small>
+  </label>
+);
+
 export default class AnomalySelector extends Component {
   constructor(props) {
     super(props);
@@ -24,19 +54,15 @@ export default class AnomalySelector extends Component {
         </span>
         <div class="anomaly-values">
           {anomalies.map(anomaly => (
-            <label class={anomaly === value ? "selected" : ""}>
-              <input
-                type="radio"
-                name="anomaly"
-                checked={anomaly === value}
-                value={anomaly}
-                onChange={() => {
-                  onChange(anomaly);
-                  toggle();
-                }}
-              />{" "}
-              {anomaly}
-            </label>
+            <AnomalyOption
+              value={anomaly}
+              anomaly={colorVisionData[anomaly]}
+              selected={anomaly === value}
+              onChange={() => {
+                onChange(anomaly);
+                toggle();
+              }}
+            />
           ))}
         </div>
       </div>
