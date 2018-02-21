8861f07163cea555a04e9e091ceebd7335807d27
Koen Kivits <koen@kivits.com>
2018-02-21 21:19:15 +0100

    Show message when not supported

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,19 +1,69 @@
-import { h } from "preact";
+import { h, Component } from "preact";
+
+import { isSupported, BROWSER_UNSUPPORTED, NO_CAMERA } from "../support";
 import logoUrl from "../img/logo.svg";
 
-const Introduction = props => (
-  <section {...props}>
-    <h1>
-      <span class="logo">
-        <img src={logoUrl} alt="" />
-      </span>
-      <span>Color Blindness Simulator</span>
-    </h1>
-    <p>Experience color blindness right from your browser.</p>
-    <div>
-      <a href="#app">Get started</a>
-    </div>
-  </section>
-);
+class Introduction extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasSupport: false,
+      noSupportReason: null,
+      supportChecked: false,
+    };
+  }
+
+  componentDidMount() {
+    isSupported()
+      .then(() => this.setState({ hasSupport: true, supportChecked: true }))
+      .catch(reason =>
+        this.setState({ noSupportReason: reason, supportChecked: true }),
+      );
+  }
+
+  renderSupportMessage() {
+    const { noSupportReason } = this.state;
+    let message;
+
+    switch (noSupportReason) {
+      case BROWSER_UNSUPPORTED:
+        message = "Unfortunately, your browser does not support this app.";
+        break;
+      case NO_CAMERA:
+        message =
+          "No cameras have been found for this device. Please connect a webcam or open this app on a device with cameras (like most smartphones)";
+        break;
+    }
+
+    if (message) {
+      return <p>{message}</p>;
+    }
+
+    return null;
+  }
+
+  render(props, { hasSupport, noSupportReason, supportChecked }) {
+    return (
+      <section {...props}>
+        <h1>
+          <span class="logo">
+            <img src={logoUrl} alt="" />
+          </span>
+          <span>Color Blindness Simulator</span>
+        </h1>
+        <p>Experience color blindness right from your browser.</p>
+
+        {this.renderSupportMessage()}
+        {supportChecked &&
+          hasSupport && (
+            <div>
+              <a href="#app">Get started</a>
+            </div>
+          )}
+      </section>
+    );
+  }
+}
 
 export default Introduction;
---
+++ src/support.js
@@ -0,0 +1,19 @@
+export const BROWSER_UNSUPPORTED = 0;
+export const NO_CAMERA = 1;
+
+export const isSupported = () =>
+  new Promise((resolve, reject) => {
+    if (
+      !window.navigator.mediaDevices ||
+      !window.navigator.mediaDevices.enumerateDevices
+    ) {
+      reject(BROWSER_UNSUPPORTED);
+    }
+
+    window.navigator.mediaDevices
+      .enumerateDevices()
+      .then(list => list.filter(device => device.kind === "videoinput"))
+      .then(
+        cameras => (cameras.length > 0 ? resolve(true) : reject(NO_CAMERA)),
+      );
+  });
