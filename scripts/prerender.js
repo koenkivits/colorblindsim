b1ec26d7ca86c0bedd306e0c8c38ef49d502fbd0
Koen Kivits <koen@kivits.com>
2018-04-04 08:13:36 +0200

    Absolute URLs for graph images

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -11,6 +11,28 @@ function beforeParse(window) {
   window.inPrerender = true;
 }
 
+/**
+ * Convert Open Graph and Twitter image URLs to absolute, because they won't work otherwise.
+ */
+function absolutizeMeta(dom) {
+  const metas = dom.window.document.querySelectorAll("meta");
+  [].forEach.call(metas, meta => {
+    const metaName = meta.getAttribute("name");
+    const metaProp = meta.getAttribute("property");
+
+    if (
+      metaName === "msapplication-config" ||
+      metaName === "twitter:image" ||
+      metaProp === "og:image"
+    ) {
+      meta.setAttribute(
+        "content",
+        "https://www.colorblindsim.com/" + meta.getAttribute("content"),
+      );
+    }
+  });
+}
+
 console.log("Prerendering index.html ...");
 
 JSDOM.fromFile(filename, {
@@ -20,6 +42,7 @@ JSDOM.fromFile(filename, {
 }).then(dom => {
   const renderInterval = setInterval(() => {
     if (dom.window.document.querySelector("h1")) {
+      absolutizeMeta(dom);
       fs.writeFileSync(filename, dom.serialize());
       clearInterval(renderInterval);
       console.log("DONE.");
